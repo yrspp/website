3d62dc5015b68d5224dccc97eec8cf8413c5a438
Hannah Walters <152662167+hannah7walters@users.noreply.github.com>
2025-11-17 18:54:50 +0000

    mega update program register venue privacy

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
         this.classList.toggle("open");
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.dropdown').forEach((detail) => {
+        detail.addEventListener('toggle', function () {
+            if (this.open) {
+                document.querySelectorAll('.dropdown').forEach((other) => {
+                    if (other !== this) other.open = false;
+                });
+            }
+        });
+    });
+});
